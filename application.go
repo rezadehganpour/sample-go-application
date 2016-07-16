e6e7942ba420b03d2a7e75a6fa0cbd746419265f
rezadehganpour <rezadehganpour@gmail.com>
2016-07-16 15:28:14 -0400

    Add the server with Gorilla

--- application.go
+++ application.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"io"
+	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
@@ -10,7 +11,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Server", "A Go Web Server")
 	w.WriteHeader(200)
 	m := make(map[string]string)
-	m["value"] = "Hello World"
+	m["message"] = "Hello World"
 	response, err := json.Marshal(m)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -19,27 +20,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 	w.Write(response)
 }
 
-var mux map[string]func(http.ResponseWriter, *http.Request)
-
 func main() {
-	server := http.Server{
-		Addr:    ":8000",
-		Handler: &myHandler{},
-	}
-
-	mux = make(map[string]func(http.ResponseWriter, *http.Request))
-	mux["/"] = index
-
-	server.ListenAndServe()
-}
-
-type myHandler struct{}
-
-func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if h, ok := mux[r.URL.String()]; ok {
-		h(w, r)
-		return
-	}
-
-	io.WriteString(w, "My server: "+r.URL.String())
+	router := mux.NewRouter()
+	router.HandleFunc("/", index)
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
